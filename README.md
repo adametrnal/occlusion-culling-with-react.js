3f2c835c04ef0460fff225a3424587b4f41c6942
Adam Brill <adam.brill@zozi.com>
2014-08-06 17:04:16 -0700

    reduce for step 1

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 # Occlussion Culling with React.js 
-More to come
 
-## Intro
-TODO: fill in info about occlusion culling
+## Step 1
+In which we set up the basics of our app and a form with a few controls.
 
-TODO: fill in info about how react helps
+Q: WTF? Why can't I edit the controls.
+A: Because data is immutable in React. If something is going to change, it needs to be stored in a state.
+
 
 ## Demo
 Check out a live demo at [http://adam-brill.com/sites/occlusion_culling/](http://adam-brill.com/sites/occlusion_culling/)
-
-TODO: explain how to use the app and why it's cool
 
 ### Credits
 Inspired by an excellent  post from James Long called [Removing User Interface Complexity, or Why React is Awesome](http://jlongster.com/Removing-User-Interface-Complexity,-or-Why-React-is-Awesome) and an [inspiring talk] by Laszlo Pandy about Immediate Mode rendering on the web(http://prezi.com/bubbzkhivh7d/immediate-mode-rendering-on-the-web/) 
--- js/app.jsx
+++ js/app.jsx
@@ -1,50 +1,7 @@
  /** @jsx React.DOM */
 
 var App = React.createClass({
-  getInitialState: function(){
-    return({
-      numberOfMurrays: 10,
-      murrayWidth:150,
-      murrayHeight:100,
-      quotes:[{quote:''}]
-    })
-  },
-
-  componentDidMount: function(){
-    this.getQuotesForMurrays();
-  },
-
-  updateMurrayWidth: function(e){
-    this.setState({murrayWidth: +e.target.value })
-  },
-
-  updateMurrayHeight: function(e){
-    this.setState({murrayHeight: +e.target.value })
-  },
-
-  updateNumberOfMurrays: function(e){
-    this.setState({numberOfMurrays: +e.target.value })
-  },
-
-  getQuotesForMurrays: function() {
-    var that = this;
-    $.get('/api/quotes.json')
-     .done(function(data){
-       that.setState({
-         quotes: data
-       });
-     })
-  },
-
-  getRowData: function(){
-     var rowData = [];
-     var quotes = this.state.quotes;
-     for(var i=0; i < this.state.numberOfMurrays; i++){
-       rowData.push({quote: quotes[i % quotes.length].quote, key:i});
-     }
-     return rowData;
-  },
-
+  
   render: function(){
     return(
       <div className="container">
@@ -55,94 +12,20 @@ var App = React.createClass({
           <div className="col-xs-4">
             <label htmlFor="numberOfMurrays">Number of Bill Murrays</label>
             <input id="numberOfMurrays" type="number" className="form-control"
-                   value={this.state.numberOfMurrays} onChange={this.updateNumberOfMurrays}/>
+                   value="10" />
           </div>
           <div className="col-xs-2">
             <label htmlFor="murrayWidthInput">Width</label>
             <input id="murrayWidthInput" type="number" className="form-control"
-                   value={this.state.murrayWidth} onChange={this.updateMurrayWidth}/>
+                   value="150" onChange={this.updateMurrayWidth}/>
           </div>
           <div className="col-xs-2">
             <label htmlFor="murrayHeightInput">Height</label>
             <input id="murrayHeightInput" type="number" className="form-control"
-                   value={this.state.murrayHeight} onChange={this.updateMurrayHeight}/>
+                   value="150" onChange={this.updateMurrayHeight}/>
           </div>
         </div>
-        <MurrayTable numRows={this.state.numberOfMurrays}
-                     murrayWidth={this.state.murrayWidth}
-                     murrayHeight={this.state.murrayHeight}
-                     rowData={this.getRowData()}/>
-
       </div>
-    )
-  }
-});
-
-var MurrayTable = React.createClass({
-    getInitialState: function(){
-      return {
-        tableScrollY:0
-      }
-    },
-
-    componentDidMount: function(){
-      var that = this;
-      var tableContainer = document.querySelector('.table-container');
-      tableContainer.addEventListener('scroll', function(e) {
-        that.setState({
-          tableScrollY: Math.max(e.currentTarget.scrollTop, 0),
-        })
-      });
-    },
-
-    render: function(){
-      var murrayWidth = this.props.murrayWidth,
-          murrayHeight = this.props.murrayHeight,
-          rowData = this.props.rowData;
-
-      var tableScrollY = +this.state.tableScrollY,
-          rowHeight = +murrayHeight + 10,
-          tableHeight = rowHeight * 4,
-          firstItem = tableScrollY / rowHeight | 0,
-          lastItem = firstItem + (tableHeight / rowHeight | 0) + 2,
-          offset = tableScrollY % rowHeight;
-
-      var fullTableHeight = {height: this.props.numRows * (murrayHeight + 10)}
-
-      var murrayRows = rowData.slice(firstItem, lastItem).map(function(murrayRow){
-        return <MurrayRow quote={murrayRow.quote}
-                          key={murrayRow.key}
-                          murrayWidth={murrayWidth}
-                          murrayHeight={murrayHeight}/>;
-      });
-
-      return(
-        <div className="row table-container" style={{height: tableHeight}} >
-          <div className="outer-table"  style={fullTableHeight} >
-            <table className="inner-table table table-bordered table-striped table-condensed" style={{top: tableScrollY}}>
-              <tbody>
-                {murrayRows}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      )
-    }
-});
-
-var MurrayRow = React.createClass({
-
-  render: function(){
-    return(
-      <tr key={this.props.key}>
-        <td>{this.props.key + 1}</td>
-        <td>
-          <img src={"http://fillmurray.com/" + this.props.murrayWidth + "/" + this.props.murrayHeight}
-               width={this.props.murrayWidth}
-               height={this.props.murrayHeight}/>
-        </td>
-        <td>{this.props.quote}</td>
-      </tr>
     )
   }
 });
